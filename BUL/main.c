70834112ea3f89206431a012f76316fe53464fae
Sgw32 <sgw32@yandex.ru>
2018-08-15 19:28:57 +0300

    readable bugfix + pwm condition

--- BUL/main.c
+++ BUL/main.c
@@ -12,12 +12,12 @@
 #define F_CPU 4800000
 #include <util/delay.h>
 #include <avr/io.h>
-
+#include <avr/interrupt.h>
 
 #define PWMOUT1 PORTB1 
 #define PWMOUT2 PORTB0 
-#define PWMCTL PORTB4
- 
+#define PWMCTL PORTB3
+#define TESTPAD PORTB2 
  
 //1100 1550
  
@@ -37,7 +37,41 @@
 #define DOOR_CLOSE_PWM 140
 #define DOOR_TIMESTEP 22 //(2000/12)
 
+#define TCNT0_US_PER_TICK 1000/75
+
 int door_status; 
+int cmd_door_open;
+int l_cmd_door_open;
+int state = 0;
+uint8_t pwmint = 0;
+
+ISR(PCINT0_vect)
+{
+	if (((PINB & (1<<PWMCTL)))) // начался фронт ШИМа 00000000000......11
+	{
+		pwmint = TCNT0; //254
+	}
+	if (!((PINB & (1<<PWMCTL))))// кончился +pulse ШИМа 1111111111111....0
+	{
+		long lpwm;
+		pwmint = TCNT0-pwmint;//140-254
+		lpwm=(long)pwmint*TCNT0_US_PER_TICK;
+		if (lpwm>1000 && lpwm<2000)
+		{
+			if (lpwm>1500)
+			{
+				PORTB&=~(1<<TESTPAD);
+				cmd_door_open = 1;
+			}
+			else
+			{
+				PORTB|=(1<<TESTPAD);
+				cmd_door_open = 0;
+			}
+		}		
+	}
+}
+ 
  
 void gpio_setup (void)
 {
@@ -73,13 +107,23 @@ void pwm_write2 (int val)
 {
 	OCR0B = val;
 }
+
+void setupint()
+{
+	 GIMSK |= (1<<PCIE); // Разрешаем внешние прерывания PCINT0.
+	 PCMSK |= (1<<PCINT3); // Разрешаем по маске прерывания на ногак кнопок (PCINT3, PCINT4)
+	 sei(); // Разрешаем прерывания глобально: SREG |= (1<<SREG_I)
+}
  
 int main (void)
 {
-    int cmd_door_open;
+    
 	door_status = 0; //Closed;
+	cmd_door_open = 0;
+	l_cmd_door_open=0;
+	
     // PWMOUT is an output.
-    DDRB |= (1 << PWMOUT1) | (1 << PWMOUT2);
+    DDRB |= (1 << PWMOUT1) | (1 << PWMOUT2) | (1 << TESTPAD);
 	// PWMCTL is an input  
 	DDRB &= ~(1 << PWMCTL);  
 	
@@ -88,15 +132,18 @@ int main (void)
   
 	pwm_write1(KEY_CLOSE_PWM);
 	pwm_write2(DOOR_CLOSE_PWM);
-  
+    
+	setupint();
+	
     while (1) {
         // Get the ADC value
-        cmd_door_open = gpio_read();
+        //cmd_door_open = gpio_read();
         // Open routine
 		if ((cmd_door_open)&&(door_status==0))
 		{
+			l_cmd_door_open = cmd_door_open;
 			_delay_ms(100);
-			if (cmd_door_open == gpio_read())
+			if (cmd_door_open == l_cmd_door_open)
 			{
 				//Lets open the key
 				for (int i=KEY_CLOSE_PWM;i!=KEY_OPEN_PWM;i--)
@@ -119,8 +166,9 @@ int main (void)
 		//Close routine
 		if ((!cmd_door_open)&&(door_status==1))
 		{
+			l_cmd_door_open = cmd_door_open;
 			_delay_ms(100);
-			if (cmd_door_open == gpio_read())
+			if (cmd_door_open == l_cmd_door_open)
 			{
 				for (int i=DOOR_OPEN_PWM;i!=DOOR_CLOSE_PWM;i++)
 				{
